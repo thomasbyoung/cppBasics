ec3b8ed859f93ca5967f645b211c23e953869768
thomasbyoung <thomasbyoung12345@gmail.com>
2022-02-01 15:23:46 -0600

    fresh basics

---
+++ Part-1/MyFirstApp/MyFirstApp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std; //namespace so do not need to use 'std::' 
+
+//int main()
+//{
+//    //Basic console output 'cout'
+//    cout << "Hello World!\n";
+//    cout << "My name is Tom." <<endl;
+//    cout << "Happy to learn C++";
+//
+//    system("pause>0");
+//}
+
+int main()
+{
+    //Basic's of variables
+
+    int red = 24;
+    cout << red;
+
+    system("pause>0");
+}
